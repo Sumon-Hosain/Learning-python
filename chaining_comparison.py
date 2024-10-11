34e1891bb21774f63ec469d25055331b006e881a
Sumon Hosain <sumonhosain0061@gmail.com>
2024-10-11 19:04:17 +0600

    Chainig Comprison

---
+++ chaining_comparison.py
@@ -0,0 +1,12 @@
+'''age=22
+if age >= 18 and age >= 28:
+    print("Eligible")                        #Normal method
+else:
+    print("Not Eligible")'''
+
+#Using chaining 
+age=22
+if  18 <= age <= 28 :
+    print("Eligible")
+else:
+    print("Not Eligible")
---
+++ ternary_operator.py
@@ -0,0 +1,2 @@
+age=int(input('Entre your age: '))
+print('Can vote') if age>=18 else print("Can't vote")
