ad60eb708b616c8eb02516b4e36235f374f23625
Sumon Hosain <sumonhosain0061@gmail.com>
2024-10-11 18:21:56 +0600

    input function

---
+++ Reaciving_input.py
@@ -0,0 +1,3 @@
+name=input('Entre your name: ')
+Fav_color=input('Entre your favourite colour:')
+print(f'{name}\'s favourite color is {Fav_color} ')
--- frist.py
+++ frist.py
@@ -1,1 +1,14 @@
+my_dict={
+    'sumon':[20,30,50],
+    'mamun':[30,40,50],
+    'ram':[40,45,50]
+}
+for everthing in my_dict.items():  #for accesssing everthing of dictionaries
+    print(everthing)
 
+for key in my_dict.keys():    # for accssing key of dictionaries
+    print(key)
+
+
+for value in my_dict.values():  #for accessing values of dicttionaries
+    print(value)
