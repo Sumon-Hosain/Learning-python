09d05d732b8d3ca06cdf6d65306d16f0bbcbd822
Sumon Hosain <sumonhosain0061@gmail.com>
2024-10-12 16:14:22 +0600

    adding item is list

---
+++ Practice.py
@@ -0,0 +1,11 @@
+list=['apple','orange','banana']
+
+list2=['Jackfruit','jack']
+
+list.extend(list2)         #
+
+list.append('sumon')       #addig item through append
+
+list.insert(1,'Hosain')
+
+print(list)
