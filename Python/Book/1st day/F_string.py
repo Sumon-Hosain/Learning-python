b4df1f4c0fc473eb086caf36a48be8f5f9693f1c
Sumon Hosain <sumonhosain0061@gmail.com>
2024-11-04 16:57:47 +0600

    Hello

---
+++ Python/Book/1st day/F_string.py
@@ -0,0 +1,4 @@
+full_name=f'{'Sumon'} {'Hosain'}'
+print(full_name)
+print('\tSumon')   #adding space with '\t' 
+print('Python\nC\nJava\nReact')
---
+++ Python/Book/1st day/String.py
@@ -0,0 +1,4 @@
+name='sumon hosain'
+print(name.title())
+print(name.upper())
+print(name.lower())
