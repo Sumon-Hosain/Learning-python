a9c1a606e5a223c8758567089b2184c306505bac
Sumon Hosain <sumonhosain0061@gmail.com>
2024-11-05 12:48:09 +0600

    '

--- Pandas/Data_Frame/Load_file.py
+++ Pandas/Data_Frame/Load_file.py
@@ -1,6 +1,6 @@
 import pandas as pd
 var=pd.read_csv('C:\\Users\\sumon\\Downloads\\data.csv')
 #print(var.to_string())               # 'to_string()' for showing entire DataFrame
-pd.options.display.max_columns=4     # This line for changeing   
+pd.options.display.max_columns=4      # This line for changing  rows and coloumns
 pd.options.display.max_rows=169 
 print(var)
