25a0ae9f3c22f0a391fbea678444408efb482e41
Sumon Hosain <sumonhosain0061@gmail.com>
2024-11-07 16:59:51 +0600

    '

--- Pandas/Cleaning_Data/dropna_method.py
+++ Pandas/Cleaning_Data/dropna_method.py
@@ -1,5 +1,5 @@
 import pandas as pd
 pd.options.display.max_rows=170
 var=pd.read_csv('E:\\Machine Learning Document\\Csv and Other documents\\data.csv')
-var.dropna(inplace=True)
+var.dropna(inplace=True)           #'inplace=True' for changing the orginal document with null values
 print(var)
